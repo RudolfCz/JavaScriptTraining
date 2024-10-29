cf028464c261a8318facc8fdbaa858cf0cead4e5
mpankiewicz <marcin.pankiewicz@gmail.com>
2024-10-29 10:44:02 +0100

    first commit

---
+++ 1-3/script.js
@@ -0,0 +1,10 @@
+let js = "amazing";
+if(js=='amazing') alert('JavaScript is fun!');
+
+console.log(40+19+23-13);
+console.log("Jonas");
+
+let firstName = "Bob";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
