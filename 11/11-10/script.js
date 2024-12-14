d58cc59e5a814f882693a3089e0104e1002460cf
Rudolf Czernichowski <rudolf.czernichowski@inpay.pl>
2024-12-14 11:44:52 +0100

    Section 11 Exercise 10 added

---
+++ 11/11-10/script.js
@@ -0,0 +1,17 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some((mov) => mov === -130));
+
+// EVERY
+console.log(movements.every((mov) => mov > 0));
+
+// Separate callback
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
