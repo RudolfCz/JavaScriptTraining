bdf6bf990043bea618eabd99aceada64bd93c456
Rudolf Czernichowski <rudolf.czernichowski@inpay.pl>
2024-12-16 20:14:20 +0100

    Section 12 Exercise 7 update

--- 12/12-7/script.js
+++ 12/12-7/script.js
@@ -24,9 +24,9 @@ const options = {
   style: "currency",
   unit: "celsius",
   currency: "EUR",
-  // useGrouping: false,
 };
 
 console.log("US:      ", new Intl.NumberFormat("en-US", options).format(num));
 console.log("Germany: ", new Intl.NumberFormat("de-DE", options).format(num));
 console.log("Syria:   ", new Intl.NumberFormat("ar-SY", options).format(num));
+console.log("Bulgaria:   ", new Intl.NumberFormat("bg-BG", options).format(num));
