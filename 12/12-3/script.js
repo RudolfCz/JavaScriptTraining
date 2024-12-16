82078190cf59054cbdb3736de11eb9119ee43583
Rudolf Czernichowski <rudolf.czernichowski@inpay.pl>
2024-12-16 18:42:30 +0100

    Section 12 Exercise 3 added

---
+++ 12/12-3/script.js
@@ -0,0 +1,21 @@
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+const isOdd = (n) => n % 2 !== 0;
+console.log(isOdd(8));
+console.log(isOdd(23));
+console.log(isOdd(514));
