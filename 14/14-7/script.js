ef90f1b505c56b37452008346afcb750011fb165
Rudolf Czernichowski <rudolf.czernichowski@inpay.pl>
2024-12-31 15:10:20 +0100

    Section 13 Exercise 7 added

---
+++ 14/14-7/script.js
@@ -0,0 +1,60 @@
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log("Hey there 👋");
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2024 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2024 - this.birthYear + 100
+      }`
+    );
+  }
+}
+
+const rudolf = new StudentCl(
+  "Rudolf Czernichowski",
+  2002,
+  "mechatronic engineering"
+);
+rudolf.introduce();
+rudolf.calcAge();
